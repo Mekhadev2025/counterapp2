0fb970bc083feb22f4fa3955b558582d451e29fe
Mekhadev2025 <98373907+Mekhadev2025@users.noreply.github.com>
2022-05-01 17:54:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Button Activators
+var plusButton = document.getElementById("increase-count");
+var minusButton = document.getElementById("decrease-count");
+
+// Counter Activator
+var counter = document.getElementById("count");
+
+// Function to increase count by one
+plusButton.addEventListener("mouseover", function() {
+    var counterInteger = parseInt(counter.innerHTML)
+    var increasedValue = counterInteger + 1;
+    counter.innerHTML = increasedValue;
+});
+ 
+// Function to reduce count by one
+minusButton.addEventListener("mouseover", function() {
+    var counterInteger = parseInt(counter.innerHTML)
+    var decreasedValue = counterInteger - 1;
+    counter.innerHTML = decreasedValue;
+});
